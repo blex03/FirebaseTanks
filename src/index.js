5cd84864346cf90b6c05ade4e7f38e414fedf200
Bryan Lecza <brylex0324@gmail.com>
2022-12-22 18:42:48 -0500

    React Overhaul

--- src/index.js
+++ src/index.js
@@ -1,119 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { 
-  getFirestore, collection, onSnapshot, 
-  addDoc, deleteDoc, doc 
-} from "firebase/firestore";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { 
-  getAuth, createUserWithEmailAndPassword, signOut,
-  signInWithEmailAndPassword
-} from "firebase/auth";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAKCJjF2FRd00lhdSFmHCuvvQ0ZusaI8hk",
-  authDomain: "fir-tanks.firebaseapp.com",
-  databaseURL: "https://fir-tanks-default-rtdb.firebaseio.com",
-  projectId: "fir-tanks",
-  storageBucket: "fir-tanks.appspot.com",
-  messagingSenderId: "210068901275",
-  appId: "1:210068901275:web:46da70a750e04c2c6fe53e",
-  measurementId: "G-RTKWB1TC9F"
-};
-
-//init firebase app
-initializeApp(firebaseConfig);
-
-//init services
-const db = getFirestore();
-const auth = getAuth();
-
-//collection ref
-const colRef = collection(db, "Players");
-
-//realtime collection data
-onSnapshot(colRef, (snapshot) => {
-  let players = []
-  snapshot.docs.forEach((doc) => {
-    players.push({ ...doc.data(), id: doc.id })
-  })
-  console.log(players)
-})
-
-const addPlayerForm = document.querySelector('.add')
-addPlayerForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  addDoc(colRef, {
-    Name: addPlayerForm.name.value,
-    Email: addPlayerForm.email.value,
-  })
-
-  .then(() => {
-    addPlayerForm.reset()
-  })
-
-
-})
-
-const deletePlayerForm = document.querySelector('.delete')
-deletePlayerForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const docRef = doc(db, "Players", deletePlayerForm.id.value)
-
-  deleteDoc(docRef)
-
-  .then(() => {
-    deletePlayerForm.reset()
-  })
-
-})
-
-const signUpForm = document.querySelector('.signup')
-signUpForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const email = signUpForm.email.value
-  const password = signUpForm.password.value
-
-
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((cred) => {
-    console.log('User Created:', cred.user)
-    signUpForm.reset()
-  })
-
-  .catch((err) => {
-    console.log(err.message)
-  })
-
-})
-
-//Log in and out
-const logOutButton = document.querySelector('.logout')
-logOutButton.addEventListener('click', () => {
-  signOut(auth)
-    .then(() => {
-      console.log('User Logged Out')
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-
-})
-const logInForm = document.querySelector('.login')
-logInForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-
-  const email = logInForm.email.value
-  const password = logInForm.password.value
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((cred) => {
-      console.log('User Logged In:', cred.user)
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
