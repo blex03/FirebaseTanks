9d7e3a5d79767df38b7d7754a48daf8ce7074935
Bryan Lecza <brylex0324@gmail.com>
2022-12-27 20:18:30 -0500

    Toggle Log In

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const signUp = () => {
+    setLoggedIn(true)
+    console.log("Signed Up")
+  }
+
+  const logIn = () => {
+    setLoggedIn(true)
+    console.log("Logged In") 
+  }
+
+  const logOut = () => {
+    console.log("Logged Out")
+    setLoggedIn(false)
+  }
 
 
   return (
     <div className="App">
-      <button onClick = {() => {console.log("It Works!")}}>BUTTON</button>
+      {!loggedIn && (
+        <div>
+          <button onClick={signUp}>Sign Up</button>
+          <button onClick={logIn}>Log In</button>
+        </div>
+      )}
+
+      {loggedIn && (
+        <div>
+          <button onClick={logOut}>Log Out</button>
+        </div>
+      )}
+
     </div>
   );
 }
